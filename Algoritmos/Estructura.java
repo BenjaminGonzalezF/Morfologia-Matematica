ea1d5934fdf6fb5cdae7ae68121502283bdef3f8
Serllet <sfuentes18@alumnos.utalca.cl>
2023-09-27 19:25:19 -0300

    Algoritmo 2 y 3

--- Algoritmos/Estructura.java
+++ Algoritmos/Estructura.java
@@ -1,50 +1,45 @@
-package Algoritmos;
-import Datos.Datos;
-import ElementosEstructurantes.Tipos.*;
+package algoritmos;
 
-//Clase padre de las implementaciones de los algoritmos
+import datos.Datos;
+import elementos_estructurantes.ElementoEstructuranteBase;
+import elementos_estructurantes.Uno;
+import elementos_estructurantes.Dos;
+import elementos_estructurantes.Tres;
+
 public class Estructura {
-    protected int nElemento;
-    protected Datos datos;
+	protected int nElemento;
+	protected Datos datos;
 
-    public Estructura (int nElemento, Datos datos){
-        this.nElemento = nElemento;
-        this.datos = datos;
-    }
+	public Estructura(int nElemento, Datos datos) {
+		this.nElemento = nElemento;
+		this.datos = datos;
+	}
 
+	public ElementoEstructuranteBase aplicarElementoEstructurante(int algoritmo, int nElemento) {
 
-    public ElementoEstructuranteBase aplicarElementoEstructurante(int algoritmo, int nElemento){
-    
-        switch (nElemento) {
-            case 1:
-                Uno uno = new Uno(algoritmo,datos);
-                return uno;
-                // Realiza acciones relacionadas con uno
-/*
-            case 2:
-                //Dos dos = new Dos(algoritmo);
-                // Realiza acciones relacionadas con dos
-                //return dos;
-            case 3:
-                // Tres tres = new Tres(algoritmo,datos);
-                // Realiza acciones relacionadas con tres
-                //return tres;
-                break;
-            case 4:
-                // Cuatro cuatro = new Cuatro(algoritmo,datos);
-                // Realiza acciones relacionadas con cuatro
-                //return cuatro;
-                break;
-            case 5:
-                // Cinco cinco = new Cinco(algoritmo,datos);
-                // Realiza acciones relacionadas con cinco
-                //return cinco;
-                break;
-                */
-            default:
-                System.out.println("Opción no válida. Introduce un valor del 1 al 5.");
-        }
-        return null;
-    }
-    
+		switch (nElemento) {
+		case 1:
+			Uno uno = new Uno(algoritmo, datos);
+			return uno;
+		case 2:
+			Dos dos = new Dos(algoritmo, datos);
+			return dos;
+		case 3:
+			Tres tres = new Tres(algoritmo, datos);
+			return tres;
+		// Realiza acciones relacionadas con uno
+		/*
+		 * case 2: //Dos dos = new Dos(algoritmo); // Realiza acciones relacionadas con
+		 * dos //return dos; case 3: // Tres tres = new Tres(algoritmo,datos); //
+		 * Realiza acciones relacionadas con tres //return tres; break; case 4: //
+		 * Cuatro cuatro = new Cuatro(algoritmo,datos); // Realiza acciones relacionadas
+		 * con cuatro //return cuatro; break; case 5: // Cinco cinco = new
+		 * Cinco(algoritmo,datos); // Realiza acciones relacionadas con cinco //return
+		 * cinco; break;
+		 */
+		default:
+			System.out.println("Opción no válida. Introduce un valor del 1 al 5.");
+		}
+		return null;
+	}
 }
---
+++ ElementosEstructurantes/Tipos/Tres.java
@@ -0,0 +1,39 @@
+package elementos_estructurantes;
+
+import java.util.ArrayList;
+
+import datos.Datos;
+
+public class Tres extends ElementoEstructuranteBase {
+	// Erosion: 1 Dilacion:2
+	public Tres(int algoritmo, Datos datos) {
+		super(algoritmo, datos);
+	}
+
+	@Override
+	public ArrayList<Integer> patron(int filainicio) {
+		ArrayList<Integer> primera = datos.getLista(filainicio);
+		ArrayList<Integer> segunda = datos.getLista(filainicio + 1);
+		ArrayList<Integer> nuevaLista = new ArrayList<Integer>();
+		nuevaLista.add(primera.get(0));
+
+		int maximoLargo = listaMenor(primera, segunda).size();
+
+		for (int i = 1; i < maximoLargo - 1; i++) {
+			int valorAReemplazar = primera.get(i);
+			int valorIzquierda = primera.get(i - 1);
+			int valorDerecha = primera.get(i + 1);
+			int nuevoValor = 0;
+
+			if (algoritmo == 1)
+				;
+			nuevoValor = menor(valorAReemplazar, valorIzquierda, valorDerecha);
+			if (algoritmo == 2)
+				;
+			nuevoValor = mayor(valorAReemplazar, valorIzquierda, valorDerecha);
+
+			nuevaLista.add(nuevoValor);
+		}
+		return nuevaLista;
+	}
+}
